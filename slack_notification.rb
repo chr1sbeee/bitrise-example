9f3f6ab505885ae396326defc3cbf2a1c20f5ace
Chris Blackmore <chris.blackmore@asos.com>
2018-03-23 13:09:17 +0000

    More updates

--- slack_notification.rb
+++ slack_notification.rb
@@ -5,10 +5,20 @@ require 'pp'
 # Prints env variables, remove at some point
 pp ENV
 
+# Map emails to slack
+teamMembers = {
+  'chris.blackmore@asos.com' => '@chris.blackmore',
+}
+
 # Constants
 slackChannel = "bitrise-slack-test"
 pullRequestURL = ENV['BITRISEIO_PULL_REQUEST_REPOSITORY_URL']
+buildLogURL = ENV['BITRISE_BUILD_URL']
 isPullRequest = pullRequestURL != nil
+didFailBecauseOfTests = ENV['BITRISE_XCODE_TEST_RESULT'] == "failed"
+authorEmailAddress = ENV['GIT_CLONE_COMMIT_AUTHOR_EMAIL']
+authorSlackUsername = teamMembers[authorEmailAddress]
+branch = ENV['BITRISE_GIT_BRANCH']
 
 # Slack setup
 Slack.configure do |config|
@@ -17,8 +27,12 @@ end
 client = Slack::Web::Client.new
 client.auth_test
 
-if (isPullRequest)
-    client.chat_postMessage(channel: slackChannel, text: "Pull request finished.", as_user: true)
-else
-    # Message if failed
+if (didFailBecauseOfTests)
+    if (isPullRequest)
+        slackMessage = "@#{authorSlackUsername} your PR has fail testing - see #{buildLogURL} for more information."
+        client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
+    else
+        slackMessage = "@here #{branch} has failed testing - see #{buildLogURL} for more information."
+        client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
+    end
 end
