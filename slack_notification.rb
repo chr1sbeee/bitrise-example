320f18c809521b63975744674331e2c33edd429a
Chris Blackmore <chris.blackmore@asos.com>
2018-03-23 15:36:19 +0000

    Updates function

--- slack_notification.rb
+++ slack_notification.rb
@@ -5,20 +5,25 @@ require 'pp'
 # Prints env variables, remove at some point
 pp ENV
 
-# Map emails to slack
+# Slack constants
 teamMembers = {
   'chris.blackmore@asos.com' => 'chris.blackmore',
 }
-
-# Constants
-slackChannel = "bitrise-slack-test"
-pullRequestURL = ENV['BITRISEIO_PULL_REQUEST_REPOSITORY_URL']
-buildLogURL = ENV['BITRISE_BUILD_URL']
-isPullRequest = pullRequestURL != nil
-didFailBecauseOfTests = ENV['BITRISE_XCODE_TEST_RESULT'] == "failed"
 authorEmailAddress = ENV['GIT_CLONE_COMMIT_AUTHOR_EMAIL']
 authorSlackUsername = teamMembers[authorEmailAddress]
+
+# URL's
+pullRequestURL = ENV['BITRISEIO_PULL_REQUEST_REPOSITORY_URL']
+buildLogURL = ENV['BITRISE_BUILD_URL']
+
+# Branch constants
 branch = ENV['BITRISE_GIT_BRANCH']
+isBuiltFromDevelop = branch == "develop"
+isBuiltFromRelease = branch.start_with?("release") 
+isPullRequest = pullRequestURL != nil
+didFailBecauseOfTests = ENV['BITRISE_XCODE_TEST_RESULT'] == "failed"
+
+##########################################
 
 # Slack setup
 Slack.configure do |config|
@@ -27,12 +32,13 @@ end
 client = Slack::Web::Client.new
 client.auth_test
 
+# Message based on circumstances
 if (didFailBecauseOfTests)
-    if (isPullRequest)
-        slackMessage = "@#{authorSlackUsername} your PR has fail testing - see #{buildLogURL} for more information."
-        client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
-    else
-        slackMessage = "@here #{branch} has failed testing - see #{buildLogURL} for more information."
-        client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
+     if (isBuiltFromDevelop || isBuiltFromRelease)
+         slackMessage = "@`#{branch}` has failed testing, the last commiter was #{authorSlackUsername} - see #{buildLogURL} for more information."
+         client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
+     else
+         slackMessage = "@#{authorSlackUsername} your PR has fail testing - see #{buildLogURL} for more information."
+         client.chat_postMessage(channel: "bitrise-slack-test", text: slackMessage, as_user: true)
     end
 end
