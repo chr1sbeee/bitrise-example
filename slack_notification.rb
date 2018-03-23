09eac78e52b9419f81457e57cb0da5fd0b10af8c
Chris Blackmore <chris.blackmore@asos.com>
2018-03-23 15:53:51 +0000

    Change made

--- slack_notification.rb
+++ slack_notification.rb
@@ -35,7 +35,7 @@ client.auth_test
 # Message based on circumstances
 if (didFailBecauseOfTests)
      if (isBuiltFromDevelop || isBuiltFromRelease)
-         slackMessage = "@`#{branch}` has failed testing, the last commiter was @#{authorSlackUsername} - see #{buildLogURL} for more information."
+         slackMessage = "`#{branch}` has failed testing, the last commiter was @#{authorSlackUsername} - see #{buildLogURL} for more information."
          client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
      else
          slackMessage = "@#{authorSlackUsername} your PR has failed testing - see #{buildLogURL} for more information."
