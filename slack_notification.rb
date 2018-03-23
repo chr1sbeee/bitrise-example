aab4aa6714253a2f80e44ff3efdffa707904547c
Chris Blackmore <chris.blackmore@asos.com>
2018-03-23 12:04:33 +0000

    Adds slack reporting

---
+++ slack_notification.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'slack-ruby-client'
+require 'pp'
+
+# Constants
+slackChannel = "br-slack-test"
+slackFailureMessage = "*Build finished.*"
+
+# Slack setup
+Slack.configure do |config|
+    config.token = ENV['SLACK_API_TOKEN']
+end
+client = Slack::Web::Client.new
+client.auth_test
+
+# Configure message
+slackMessage = slackFailureMessage
+#slackMessage = "#{slackMessage}\nIPA Path: #{ipaPath}"
+#slackMessage = "#{slackMessage}\nPR: #{pullRequestURL}"
+#slackMessage = "#{slackMessage}\nTrigger: #{trigger}"
+#slackMessage = "#{slackMessage}\nLog: #{buildLogURL}"
+
+# Prints env variables, remove at some point
+pp ENV
+
+# Send
+client.chat_postMessage(channel: slackChannel, text: slackMessage, as_user: true)
