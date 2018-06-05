22c4cf8c3f009b0d0cc3cc121d9f2e7d988e617a
Chris Blackmore <chris.blackmore@asos.com>
2018-06-05 10:00:49 +0100

    burgh

--- slack_notification.rb
+++ slack_notification.rb
@@ -1,28 +1,30 @@
 #!/usr/bin/env ruby
 require 'slack-ruby-client'
-require 'pp'
 
-# Prints env variables, remove at some point
+# TODO: REMOVE
+require 'pp'
 pp ENV
 
-# Map emails to slack
+# ENV
+pullRequestURL = ENV['BITRISEIO_PULL_REQUEST_REPOSITORY_URL']
+buildLogURL = ENV['BITRISE_BUILD_URL']
+authorEmailAddress = ENV['GIT_CLONE_COMMIT_AUTHOR_EMAIL']
+branch = ENV['BITRISE_GIT_BRANCH']
+testResult = ENV['BITRISE_XCODE_TEST_RESULT']
+slackAPIToken = ENV['SLACK_API_TOKEN']
+
 teamMembers = {
   'chris.blackmore@asos.com' => 'chris.blackmore',
 }
 
-# Constants
 slackChannel = "bitrise-slack-test"
-pullRequestURL = ENV['BITRISEIO_PULL_REQUEST_REPOSITORY_URL']
-buildLogURL = ENV['BITRISE_BUILD_URL']
 isPullRequest = pullRequestURL != nil
-didFailBecauseOfTests = ENV['BITRISE_XCODE_TEST_RESULT'] == "failed"
-authorEmailAddress = ENV['GIT_CLONE_COMMIT_AUTHOR_EMAIL']
+didFailBecauseOfTests = testResult == "failed" # "failed" if test fails or it fails to compile
 authorSlackUsername = teamMembers[authorEmailAddress]
-branch = ENV['BITRISE_GIT_BRANCH']
 
 # Slack setup
 Slack.configure do |config|
-    config.token = ENV['SLACK_API_TOKEN']
+    config.token = slackAPIToken
 end
 client = Slack::Web::Client.new
 client.auth_test
@@ -37,7 +39,11 @@ if (didFailBecauseOfTests)
     end
 end
 
-
-
-
-
+counter = 1
+file = File.new("/var/folders/90/5stft2v13fb_m_gv3c8x9nwc0000gn/T/bitrise757480219/formatted_output.md", "r")
+puts file
+while (line = file.gets)
+    puts line
+    counter = counter + 1
+end
+file.close 
